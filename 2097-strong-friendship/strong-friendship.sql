3931e8aedcf3f65b05be6c4636f5165302bf4f3e
Rahel Paul Fargose <43895884+rfargose@users.noreply.github.com>
2024-03-17 14:06:00 -0400

    Time: 545 ms (97.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2097-strong-friendship/strong-friendship.sql
@@ -0,0 +1,17 @@
+with f as (
+select user1_id, user2_id 
+from Friendship
+union 
+select user2_id user1_id, user1_id user2_id
+from Friendship
+)
+
+select a.user1_id, a.user2_id, count(c.user2_id) common_friend
+from Friendship a 
+join f b 
+on a.user1_id = b.user1_id # u1 friends
+join f c 
+on a.user2_id = c.user1_id # u2 friends
+and b.user2_id = c.user2_id # u1 u2 comman friends
+group by a.user1_id, a.user2_id
+having count(c.user2_id) >= 3

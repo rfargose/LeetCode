8f5d42bc4e32b34a552e2d3bfaa4b10bb9b4185b
Rahel Paul Fargose <43895884+rfargose@users.noreply.github.com>
2024-08-12 13:06:35 -0400

    Time: 757 ms (16.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,5 @@
+select date_format(trans_date, '%Y-%m') month, country, count(id) trans_count,
+sum(state = 'approved') approved_count, sum(amount) trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) approved_total_amount
+from transactions
+group by 1, 2

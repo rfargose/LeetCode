abbf8593e03c5aad70a1e0b692e15fb70bf153d8
Rahel Paul Fargose <43895884+rfargose@users.noreply.github.com>
2024-08-16 11:14:39 -0400

    Time: 972 ms (45.09%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,9 +1,8 @@
-with cte1 as (
-  select d.name Department, e.name Employee, e.salary Salary, dense_rank() over(partition by departmentId order by salary desc) r
-  from employee e
-  left join department d
-  on e.departmentId = d.id
-)
+with cte1 as (select d.name Department, e.name Employee, e.salary Salary, 
+dense_rank() over(partition by e.departmentId order by salary desc) as rk
+from employee e 
+inner join department d on e.departmentId = d.id)
 
-select Department, Employee, Salary from cte1
-where r <= 3
+select Department, Employee, Salary
+from cte1
+where rk < 4

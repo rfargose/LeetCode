87000629a2c7eb6dcd0c7cc2e4f2f81d1d8198ed
Rahel Paul Fargose <43895884+rfargose@users.noreply.github.com>
2024-08-16 12:27:51 -0400

    Time: 1606 ms (62.83%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,15 +1,5 @@
-with cte1 as (select case when income < 20000 then 'Low Salary'
-when income >= 20000 and income <= 50000 then 'Average Salary'
-when income > 50000 then 'High Salary' end as category
-from accounts),
-
-cte2 as (select 'Low Salary' as category
+select 'Low Salary' category, sum(case when income < 20000 then 1 else 0 end) accounts_count from accounts
 union
-select 'Average Salary' as category
+select 'Average Salary' category, sum(case when income between 20000 and 50000 then 1 else 0 end) accounts_count from accounts
 union
-select 'High Salary' as category),
-
-cte3 as (select category, count(category) as ct from cte1 group by 1)
-
-select c2.category, coalesce(c3.ct, 0) accounts_count
-from cte2 c2 left join cte3 c3 on c2.category = c3.category
+select 'High Salary' category, sum(case when income > 50000 then 1 else 0 end) accounts_count from accounts

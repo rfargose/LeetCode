62a9976bda336ea6d986748dceb5987c88792678
Rahel Paul Fargose <43895884+rfargose@users.noreply.github.com>
2024-08-20 11:57:11 -0400

    Time: 291 ms (62.04%) | Memory: 0B (100.00%) - LeetSync

--- 3564-premier-league-table-ranking-ii/premier-league-table-ranking-ii.sql
+++ 3564-premier-league-table-ranking-ii/premier-league-table-ranking-ii.sql
@@ -4,13 +4,8 @@ rank() over(order by (wins * 3 + draws * 1) desc) position
 from teamstats)
 
 select team_name, points, position,
-CASE
-         WHEN position < (0.33 * (SELECT MAX(position) FROM cte1)+1) THEN 'Tier 1'
-         ELSE
-         CASE
-            WHEN position < (0.66 * (SELECT MAX(position) FROM cte1)+1) THEN 'Tier 2'
-            ELSE 'Tier 3'
-         END
-    END AS tier
+CASE WHEN position < (0.33 * (SELECT MAX(position) FROM cte1)+1) THEN 'Tier 1'
+WHEN position < (0.66 * (SELECT MAX(position) FROM cte1)+1) THEN 'Tier 2'
+ELSE 'Tier 3' END AS tier
 from cte1
 order by points desc, team_name

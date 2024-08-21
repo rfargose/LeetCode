4d96490f4d412692a1283edf100445b4d7d3c30c
Rahel Paul Fargose <43895884+rfargose@users.noreply.github.com>
2024-08-21 11:22:23 -0400

    Time: 1102 ms (48.10%) | Memory: 0B (100.00%) - LeetSync

--- 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -1,15 +1,12 @@
-with cte1 as (select e.*, v.value v1, v1.value v2 
+with cte1 as (select e.*,
+case when operator = '=' then v.value = v1.value 
+when operator = '<' then v.value < v1.value
+when operator = '>' then v.value > v1.value
+end as value
 from expressions e 
 inner join variables v on e.left_operand = v.name
-inner join variables v1 on e.right_operand = v1.name),
-
-cte2 as (select c1.*,
-case when operator = '=' then v1 = v2 
-when operator = '<' then v1 < v2
-when operator = '>' then v1 > v2
-end as value
-from cte1 c1)
+inner join variables v1 on e.right_operand = v1.name)
 
 select left_operand, operator, right_operand,
 case when value = 1 then 'true' else 'false' end as value
-from cte2
+from cte1

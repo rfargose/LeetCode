42818d8d9734e212b3726b06bed391aff8cba625
Rahel Paul Fargose <43895884+rfargose@users.noreply.github.com>
2024-08-21 11:56:52 -0400

    Time: 246 ms (45.31%) | Memory: 0B (100.00%) - LeetSync

--- 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -1,10 +1,13 @@
-with recursive cte as (
-    select employee_id, employee_name, manager_id, 1 as level from employees
-    WHERE manager_id = 1 AND employee_id <> 1
+with recursive cte1 as (
+    select employee_id, employee_name, manager_id,
+    1 as level
+    from employees
+    where manager_id = 1 and employee_id != 1
     union all
-    select e.employee_id, e.employee_name, e.manager_id, level + 1 as level
-    from employees e inner join cte c
-    on e.manager_id = c.employee_id
+    select e.employee_id, e.employee_name, e.manager_id,
+    level + 1 as level
+    from employees e
+    inner join cte1 c1 on e.manager_id = c1.employee_id
 )
 
-select employee_id from cte
+select employee_id from cte1
